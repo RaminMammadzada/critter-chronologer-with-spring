bc3ce7ade6774f26e9ad2cd177f1151bb182ddca
Ramin Mammadzada <raminmammadzadaiu@gmail.com>
2022-08-30 10:19:54 +0300

    Add @Transactional annotation to services. Update postman collection.

--- src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
@@ -6,6 +6,7 @@ import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
 import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -16,6 +17,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
